8eb3f753256b7f1ed8a1c7f7d49fe51a67f7e1b9
Plidher <65611683+PLNSTD@users.noreply.github.com>
2024-11-12 19:27:06 -0500

    Added builtin Navigator and getPosition

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import { useState } from "react";
 
+// "I am using OpenStreetMap location"
+
 function App() {
   const [countClicks, setCountClicks] = useState(0);
+  const [userLocation, setUserLocation] = useState(null);
+  const [error, setError] = useState(null);
 
-  const handleCountClick = function () {
+  function handleCountClick() {
     setCountClicks((c) => c + 1);
-  };
+    getUserLocation();
+  }
+
+  function getUserLocation() {
+    if (navigator.geolocation) {
+      // console.log("Allowed Geolocation");
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ latitude, longitude });
+        },
+        (error) => {
+          setError("User denied Geolocation");
+        }
+      );
+    } else {
+      console.log("Geolocation NOT SUPPORTED");
+    }
+  }
 
   return (
     <div className="App">
       <button onClick={handleCountClick}>Get my position</button>
+      {error && <p>{error}</p>}
+      {userLocation && (
+        <p>
+          Your GPS position: {userLocation.latitude}, {userLocation.longitude}
+        </p>
+      )}
       <p>You requested position {countClicks} times</p>
     </div>
   );
